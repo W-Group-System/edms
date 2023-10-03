4c3fefb875d9aa7a7852036d1654ace73bac4c6e
renzcabato1 <cabato.renz.renz@gmail.com>
2023-10-03 08:24:39 +0800

    email

--- app/Http/Controllers/PermitController.php
+++ app/Http/Controllers/PermitController.php
@@ -202,18 +202,14 @@ class PermitController extends Controller
     public function email_notif()
     {
         $users = User::where('status',null)->get();
-        // dd($users);
         foreach($users as $user)
         {
             $permits = Permit::with('company', 'department')->get();
             
             if($user->role == "Document Control Officer")
             { 
-    
                 $permits = Permit::with('company', 'department')->whereIn('department_id',($user->dco)->pluck('department_id')->toArray())->get();
-                    
             }
-            
             if(($user->role == "Department Head"))
             {
                 $permits = Permit::with('company', 'department')->whereIn('department_id',($user->permits)->pluck('department_id')->toArray())->get();
@@ -222,11 +218,16 @@ class PermitController extends Controller
             {
                 $permits = Permit::with('company', 'department')->whereIn('department_id',($user->accountable_persons)->pluck('department_id')->toArray())->get();
             }
+            if(($user->role == "Documents and Records Controller"))
+            {
+                $permits = Permit::with('company', 'department')->whereIn('department_id',($user->accountable_persons)->pluck('department_id')->toArray())->get();
+            }
 
             $countPermit = count($permits->where('expiration_date','!=',null)->where('expiration_date','<',date('Y-m-d', strtotime("+3 months", strtotime(date('Y-m-d'))))));
+            $countOverdue = count($permits->where('expiration_date','!=',null)->where('expiration_date','<',date('Y-m-d')));
             if($countPermit > 0)
             {
-                $user->notify(new ForRenewal($countPermit));
+                $user->notify(new ForRenewal($countPermit,$countOverdue));
             }
         }
     }
