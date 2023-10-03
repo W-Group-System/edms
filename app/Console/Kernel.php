622ed4a5f647ade0858c226c97b9a151bbe65adb
renzcabato1 <cabato.renz.renz@gmail.com>
2023-10-03 08:20:32 +0800

    Done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:send_renewals')->twiceDaily(8, 21);
+        $schedule->command('command:send_renewals')->everyFiveMinutes();;
     }
 
     /**
