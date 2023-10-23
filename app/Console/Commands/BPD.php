a3c59228775d7b5be1c009905c9f430281c92d1b
renzcabato1 <cabato.renz.renz@gmail.com>
2023-10-23 08:56:35 +0800

    Done

--- app/Console/Commands/BPD.php
+++ app/Console/Commands/BPD.php
@@ -45,11 +45,11 @@ class BPD extends Command
         {
             $change_requests = ChangeRequest::with('approvers')->whereIn('department_id',($user->dco)->pluck('department_id')->toArray())->where('status','Pending')->get();
 
-            $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Code</th><th>Approver</th></tr>";
+            $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Date Requested</th><th>Code</th><th>Approver</th></tr>";
             foreach($change_requests as $request)
             {
                 $approver = ($request->approvers)->where('level',$request->level)->first();
-                $table .= "<tr><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
+                $table .= "<tr><td>".date('Y-m-d',strtotime($request->created_at))."</td><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
             }
             $table .= "</table>";
             if(count($change_requests) >0)
@@ -63,18 +63,17 @@ class BPD extends Command
         {
             $change_requests = ChangeRequest::with('approvers')->where('status','Pending')->get();
 
-            $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Code</th><th>Approver</th></tr>";
+            $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Date Requested</th><th>Code</th><th>Approver</th></tr>";
             foreach($change_requests as $request)
             {
                 $approver = ($request->approvers)->where('level',$request->level)->first();
-                $table .= "<tr><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
+                $table .= "<tr><td>".date('Y-m-d',strtotime($request->created_at))."</td><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
             }
             $table .= "</table>";
             if(count($change_requests) >0)
             {
                 $user->notify(new PendingRequest($table));
             }
-           
         }
       
         info("END DCO");
