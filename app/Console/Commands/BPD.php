b41fc3cf5032dd6eb0d2b237f23c2fb2d18f6cc0
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2023-12-18 15:44:01 +0800

    Notification

--- app/Console/Commands/BPD.php
+++ app/Console/Commands/BPD.php
@@ -41,41 +41,41 @@ class BPD extends Command
     {
         //
         info("START DCO");
-        // $users = User::where('status',null)->where('role','Document Control Officer')->get();
-        // foreach($users as $user)
-        // {
-        //     $change_requests = ChangeRequest::with('approvers')->whereIn('department_id',($user->dco)->pluck('department_id')->toArray())->where('status','Pending')->get();
+        $users = User::where('status',null)->where('role','Document Control Officer')->get();
+        foreach($users as $user)
+        {
+            $change_requests = ChangeRequest::with('approvers')->whereIn('department_id',($user->dco)->pluck('department_id')->toArray())->where('status','Pending')->get();
 
-        //     $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Date Requested</th><th>Code</th><th>Approver</th></tr>";
-        //     foreach($change_requests as $request)
-        //     {
-        //         $approver = ($request->approvers)->where('level',$request->level)->first();
-        //         $table .= "<tr><td>".date('Y-m-d',strtotime($request->created_at))."</td><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
-        //     }
-        //     $table .= "</table>";
-        //     if(count($change_requests) >0)
-        //     {
-        //         $user->notify(new PendingRequest($table));
-        //     }
+            $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Date Requested</th><th>Code</th><th>Approver</th></tr>";
+            foreach($change_requests as $request)
+            {
+                $approver = ($request->approvers)->where('level',$request->level)->first();
+                $table .= "<tr><td>".date('Y-m-d',strtotime($request->created_at))."</td><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
+            }
+            $table .= "</table>";
+            if(count($change_requests) >0)
+            {
+                $user->notify(new PendingRequest($table));
+            }
            
-        // }
-        // $users_d = User::where('status',null)->where('role','Business Process Manager')->orWhere('role','Management Representative')->get();
-        // foreach($users_d as $user)
-        // {
-        //     $change_requests = ChangeRequest::with('approvers')->where('status','Pending')->get();
+        }
+        $users_d = User::where('status',null)->where('role','Business Process Manager')->orWhere('role','Management Representative')->get();
+        foreach($users_d as $user)
+        {
+            $change_requests = ChangeRequest::with('approvers')->where('status','Pending')->get();
 
-        //     $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Date Requested</th><th>Code</th><th>Approver</th></tr>";
-        //     foreach($change_requests as $request)
-        //     {
-        //         $approver = ($request->approvers)->where('level',$request->level)->first();
-        //         $table .= "<tr><td>".date('Y-m-d',strtotime($request->created_at))."</td><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
-        //     }
-        //     $table .= "</table>";
-        //     if(count($change_requests) >0)
-        //     {
-        //         $user->notify(new PendingRequest($table));
-        //     }
-        // }
+            $table = "<table style='margin-bottom:10px;' width='100%' border='1' cellspacing=0><tr><th>Date Requested</th><th>Code</th><th>Approver</th></tr>";
+            foreach($change_requests as $request)
+            {
+                $approver = ($request->approvers)->where('level',$request->level)->first();
+                $table .= "<tr><td>".date('Y-m-d',strtotime($request->created_at))."</td><td>CR-".str_pad($request->id, 5, '0', STR_PAD_LEFT)."</td><td>".$approver->user->name."</td></tr>";
+            }
+            $table .= "</table>";
+            if(count($change_requests) >0)
+            {
+                $user->notify(new PendingRequest($table));
+            }
+        }
 
         $users_approvers = User::where('status',null)->get();
         foreach($users_approvers as $user)
@@ -104,7 +104,7 @@ class BPD extends Command
             
             $table .= "</table>";
 
-            if(count($change_requests) >0)
+            if((count($change_requests) >0) ||(count($copy_requests) >0))
             {
                 $user->notify(new PendingRequest($table));
             }
