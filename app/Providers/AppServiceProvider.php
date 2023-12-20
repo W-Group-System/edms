0efccd114909b7460a325fbc5ae68d66b4c9053c
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2023-12-20 18:49:47 +0800

    Done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('prod')) {
+            \URL::forceScheme('https');
+        }
         //
     }
 }
